21d8a884c6ec2b08834219d55f3f22204badf711
Paco Viromontes <pair+paco@getsatisfaction.com>
2011-12-20 10:51:02 -0800

    Removed Net::Http patch to avoid warning
    This patch might cause other libraries that patch
    Net::Http to get into a funky state

--- lib/satisfaction.rb
+++ lib/satisfaction.rb
@@ -1,13 +1,5 @@
 require 'satisfaction/external_dependencies'
-# Suppress the 'warning: peer certificate won't be verified in this SSL session'
-class Net::HTTP
-  alias_method :old_initialize, :initialize
-  def initialize(*args)
-    old_initialize(*args)
-    @ssl_context = OpenSSL::SSL::SSLContext.new
-    @ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  end
-end
+
 module Sfn
 end
 class Satisfaction
--- lib/satisfaction/version.rb
+++ lib/satisfaction/version.rb
@@ -2,7 +2,7 @@ module GemSatisfaction
   module VERSION
     MAJOR = 0
     MINOR = 7
-    PATCH = 2
+    PATCH = 3
     PRE   = nil
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
